d891c3325a3b6fc328f7768d20939015f3139a2d
Wimt <juanjo13204@hotmail.com>
2024-06-21 13:13:04 +0200

    Reformated, add comments to all the frontend

--- src/SudokuCell.tsx
+++ src/SudokuCell.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./SudokuCell.css"; // Import CSS file
+import "./SudokuCell.css"; //Import CSS file for styling
 
 interface SudokuCellProps {
   number: number;
@@ -10,10 +10,10 @@ interface SudokuCellProps {
   setGrid: React.Dispatch<React.SetStateAction<number[][]>>;
   marked: number;
   setMarked: React.Dispatch<React.SetStateAction<number>>;
-  colMarked: number;
-  setColMarked: React.Dispatch<React.SetStateAction<number>>;
   rowMarked: number;
   setRowMarked: React.Dispatch<React.SetStateAction<number>>;
+  colMarked: number;
+  setColMarked: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const SudokuCell: React.FC<SudokuCellProps> = ({
@@ -30,49 +30,55 @@ const SudokuCell: React.FC<SudokuCellProps> = ({
   colMarked,
   setColMarked,
 }) => {
+  //Function to determine the content inside the cell
   function getInside() {
     if (locked) {
+      //Display locked numbers as text
       return <div onClick={handleClick}>{number}</div>;
+    } else {
+      //Allow input for non-locked cells
+      return (
+        <input
+          type="text"
+          inputMode="numeric"
+          maxLength={1}
+          onChange={handleChange}
+          onClick={handleClick}
+          value={number !== 0 ? number : ""}
+        />
+      );
     }
-    return (
-      <input
-        type="text"
-        inputMode="numeric"
-        maxLength={1}
-        onChange={handleChange}
-        onClick={handleClick}
-        value={number != 0 ? number : ""}
-      />
-    );
   }
 
+  //Handle click on the cell to mark it and its row/column
   function handleClick() {
     setMarked(number);
     setRowMarked(row);
     setColMarked(col);
   }
 
+  //Handle change in input value (for non-locked cells)
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const value: number = +event.target.value;
     if (Number.isNaN(value)) {
       return;
     }
+    //Update the grid state with the new value
     let temp = [...grid];
     temp[row] = [...temp[row]];
     temp[row][col] = value;
-    number = value;
     setGrid(temp);
   }
 
+  //Determine dynamic classNames based on cell's properties
   const isLastInRow = col === 8;
   const isLastInColumn = row === 8;
-
   const isThickBorderRight = (col + 1) % 3 === 0 && !isLastInRow;
   const isThickBorderBottom = (row + 1) % 3 === 0 && !isLastInColumn;
+  const isMarked = number === marked;
+  const isRowColMarked = row === rowMarked || col === colMarked;
 
-  const isMarked = number == marked;
-  const isRowColMarked = row == rowMarked || col == colMarked;
-
+  //Construct the className string based on conditions
   const cellClassName = `sudoku-cell${isLastInRow ? " last-in-row" : ""}${
     isLastInColumn ? " last-in-column" : ""
   }${isThickBorderRight ? " thick-border-right" : ""}${
@@ -81,6 +87,7 @@ const SudokuCell: React.FC<SudokuCellProps> = ({
     isMarked && number ? " marked" : ""
   }${isRowColMarked ? " rowColmarked" : ""}`;
 
+  //Render the cell with appropriate className and content
   return <td className={cellClassName}>{getInside()}</td>;
 };
 
--- src/SudokuGrid.tsx
+++ src/SudokuGrid.tsx
@@ -5,24 +5,28 @@ import { message } from "@tauri-apps/api/dialog";
 import { listen } from "@tauri-apps/api/event";
 
 const SudokuGrid = () => {
+  //State hooks
   const [grid, setGrid] = useState<number[][]>([]);
   const [solved, setSolved] = useState<number[][]>([]);
   const initialLockedGrid = Array.from({ length: 9 }, () =>
     Array(9).fill(false)
   );
   const [lockedGrid, setLockedGrid] = useState<boolean[][]>(initialLockedGrid);
-
   const [marked, setMarked] = useState(0);
   const [rowMarked, setRowMarked] = useState(0);
   const [colMarked, setColMarked] = useState(0);
 
+  //Refs to hold current grid and locked grid values
   const gridRef = useRef(grid);
   const gridLockRef = useRef(lockedGrid);
+
+  //Update refs when grid or lockedGrid changes
   useEffect(() => {
     gridRef.current = grid;
     gridLockRef.current = lockedGrid;
   }, [grid, lockedGrid]);
 
+  //Fetch initial data on component mount
   useEffect(() => {
     async function fetchData() {
       const data: number[][] = await invoke("get_grid", {});
@@ -34,6 +38,7 @@ const SudokuGrid = () => {
     }
     fetchData();
 
+    //Listen for save-grid event to save grid and lockedGrid
     async function addListen() {
       await listen("save-grid", async () => {
         await invoke("save_grid", { grid: gridRef.current });
@@ -42,8 +47,9 @@ const SudokuGrid = () => {
       });
     }
     addListen();
-  }, []);
+  }, []); //Empty dependency array ensures useEffect runs only on mount
 
+  //Reset grid to initial state
   async function handleResetClick() {
     let newGrid: number[][] = await fetchGrid();
     if (testEmpty(newGrid)) {
@@ -55,6 +61,7 @@ const SudokuGrid = () => {
     setSolved(s);
   }
 
+  //Check if grid is empty
   function testEmpty(grid: number[][]) {
     for (let i = 0; i < grid.length; i++) {
       for (let j = 0; j < grid[i].length; j++) {
@@ -66,11 +73,13 @@ const SudokuGrid = () => {
     return true;
   }
 
+  //Fetch a new grid from backend
   async function fetchGrid() {
     let grid: number[][] = await invoke("generate_new_grid", {});
     return grid;
   }
 
+  //Handle check button click to compare grid with solved grid
   async function handleCheckClick() {
     let isCorrect: boolean = compareGrids(grid, solved);
     await message(
@@ -78,6 +87,7 @@ const SudokuGrid = () => {
     );
   }
 
+  //Compare two grids
   function compareGrids(g1: number[][], g2: number[][]) {
     if (g1.length !== g2.length) {
       return false;
@@ -95,6 +105,7 @@ const SudokuGrid = () => {
     return true;
   }
 
+  //Update grid and lockedGrid state
   function updateGrid(g: number[][]) {
     let empt = Array.from({ length: 9 }, () => Array(9).fill(0));
     setGrid(empt);
@@ -105,19 +116,19 @@ const SudokuGrid = () => {
     setLockedGrid(newLockedGrid);
   }
 
+  //Update grid with solved grid values
   function updateSolveGrid(g: number[][]) {
     let empt = Array.from({ length: 9 }, () => Array(9).fill(0));
     setGrid(empt);
     setGrid(g);
   }
 
+  //Handle solve button click
   async function handleSolveClick() {
-    console.log("test");
-    console.log(solved);
     updateSolveGrid(solved);
-    console.log(grid);
   }
 
+  //Render SudokuCell components based on grid state
   function getGridElements(grid: number[][]) {
     return grid.map((row, i) => (
       <tr key={`row-${i}`}>
@@ -142,6 +153,7 @@ const SudokuGrid = () => {
     ));
   }
 
+  //JSX to render SudokuGrid component
   return (
     <div>
       <table id="sudoku-grid">
