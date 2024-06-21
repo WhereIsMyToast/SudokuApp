bdc6be793974a6829e1660f22b509926b56cfc6a
Wimt <juanjo13204@hotmail.com>
2024-06-21 12:59:26 +0200

    Reformated main.rs, and added comments

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,9 @@
+//Conditional compilation attribute to configure the Windows subsystem
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+
 use serde::Serialize;
 use solver::{Grid, NewGridsData};
 use std::fs::OpenOptions;
@@ -15,24 +17,27 @@ use tauri::api::path::data_dir;
 use tauri::WindowEvent;
 
 mod solver;
+
+//Command to solve the provided Sudoku grid
 #[tauri::command]
 fn solve_grid(grid: [[u8; 9]; 9]) -> [[u8; 9]; 9] {
-    let mut pgrid = Grid::new();
-    pgrid.grid = grid;
-    if !pgrid.solve() {
-        return [[0; 9]; 9];
+    let mut solve_grid = Grid::new();
+    solve_grid.grid = grid;
+    if !solve_grid.solve() {
+        return [[0; 9]; 9]; //Return an empty grid if solving fails
     }
-    pgrid.grid
+    solve_grid.grid
 }
 
+//Command to save the current Sudoku grid to a file
 #[tauri::command]
 fn save_grid(grid: [[u8; 9]; 9]) {
-    println!("{:?}", grid);
     let mut path: String = get_appdata();
     path.push_str("/last_grid");
-    println!("{}", path);
     let _ = write_file(grid, path);
 }
+
+//Command to save the locked cells of the Sudoku grid to a file
 #[tauri::command]
 fn save_locked_grid(grid: [[bool; 9]; 9]) {
     let mut path: String = get_appdata();
@@ -40,40 +45,37 @@ fn save_locked_grid(grid: [[bool; 9]; 9]) {
     let _ = write_file(grid, path);
 }
 
+//Command to retrieve the last saved Sudoku grid from a file
 #[tauri::command]
 fn get_grid() -> [[u8; 9]; 9] {
     let mut path: String = get_appdata();
     path.push_str("/last_grid");
     match read_file::<u8>(&path) {
-        Err(e) => {
-            println!("Error reading file: {}", e);
-            return [[0; 9]; 9];
+        Err(_) => {
+            return [[0; 9]; 9]; //Return an empty grid on error
         }
-        Ok(grid) => {
-            return grid;
-        }
+        Ok(grid) => grid,
     }
 }
 
+//Command to retrieve the last saved locked cells of the Sudoku grid from a file
 #[tauri::command]
 fn get_locked_grid() -> [[bool; 9]; 9] {
     let mut path: String = get_appdata();
     path.push_str("/last_grid_locked");
     match read_file::<bool>(&path) {
-        Err(e) => {
-            println!("Error reading file: {}", e);
-
-            return [[false; 9]; 9];
+        Err(_) => {
+            return [[false; 9]; 9]; //Return an empty locked grid on error
         }
-        Ok(grid) => {
-            return grid;
-        }
+        Ok(grid) => grid,
     }
 }
 
+//Singleton instance to manage new grids
 static mut INSTANCE: Option<Mutex<NewGridsData>> = None;
 static INIT: Once = Once::new();
 
+//Function to get the singleton instance
 fn get_singleton() -> &'static Mutex<NewGridsData> {
     INIT.call_once(|| {
         let new_grids = NewGridsData::new();
@@ -84,28 +86,21 @@ fn get_singleton() -> &'static Mutex<NewGridsData> {
     unsafe { INSTANCE.as_ref().unwrap() }
 }
 
+//Command to generate a new Sudoku grid
 #[tauri::command]
 fn generate_new_grid() -> [[u8; 9]; 9] {
     let new_grids = get_singleton();
     let grids_lock_result = new_grids.lock();
     match grids_lock_result {
-        Ok(grids_guard) => {
-            let grid_option = grids_guard.get_grid();
-            match grid_option {
-                Some(grid) => grid.grid,
-                None => {
-                    println!("No grid available!");
-                    [[0; 9]; 9]
-                }
-            }
-        }
-        Err(lock_error) => {
-            println!("Error acquiring lock: {:?}", lock_error);
-            [[0; 9]; 9]
-        }
+        Ok(grids_guard) => match grids_guard.get_grid() {
+            Some(grid) => grid.grid,
+            None => [[0; 9]; 9],
+        },
+        Err(_) => [[0; 9]; 9],
     }
 }
 
+//Function to write a grid to a file
 fn write_file<T: Serialize>(g: [[T; 9]; 9], f: String) -> Result<()> {
     let file = OpenOptions::new()
         .write(true)
@@ -117,6 +112,8 @@ fn write_file<T: Serialize>(g: [[T; 9]; 9], f: String) -> Result<()> {
     writer.flush()?;
     Ok(())
 }
+
+//Function to read a grid from a file
 fn read_file<T: serde::de::DeserializeOwned>(file_dir: &str) -> Result<[[T; 9]; 9]> {
     let file: File = match File::open(file_dir) {
         Err(_) => File::create(file_dir)?,
@@ -128,23 +125,24 @@ fn read_file<T: serde::de::DeserializeOwned>(file_dir: &str) -> Result<[[T; 9]; 
     Ok(grid)
 }
 
+//Function to get the application data directory
 fn get_appdata() -> String {
     match data_dir() {
         Some(dir) => match dir.to_str() {
             None => String::new(),
             Some(d) => String::from(d),
         },
-        None => return String::new(),
+        None => String::new(),
     }
 }
 
+//Main function to initialize the application and set up Tauri commands
 fn main() {
     get_singleton();
     tauri::Builder::default()
         .on_window_event(|event| match event.event() {
             WindowEvent::CloseRequested { api, .. } => {
                 api.prevent_close();
-                println!("Prevented Close");
                 event.window().emit("save-grid", "").unwrap();
             }
             _ => {}
